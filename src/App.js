e8a80481aad1320c4ad26e5900f95bd60ce09f36
miriambourke <mir.bourke92@gmail.com>
2020-11-07 13:41:09 +0000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Router, Route } from "react-router-dom";
+
 import './App.css';
+
+import Amplify from "aws-amplify";
+import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { AmazonAIPredictionsProvider } from '@aws-amplify/predictions';
+
+import awsExports from "./aws-exports";
+
+import HomePage from './components/HomePage';
+import { createBrowserHistory as createHistory } from 'history'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import './App.css';
+
+import UploadImage from './components/UploadImage';
+
+Amplify.configure(awsExports);
+Amplify.addPluggable(new AmazonAIPredictionsProvider());
+
+const history = createHistory();
 
 function App() {
   return (
+    <AmplifyAuthenticator>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router history={history}>
+        <Navbar bg="primary" expand="lg" variant="dark" >
+          <Navbar.Brand href="/">Image Gallery App</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/uploadImage">Upload Image</Nav.Link>
+
+            </Nav>
+          </Navbar.Collapse>
+          <AmplifySignOut />
+        </Navbar>
+
+       <Route path="/" exact component={HomePage} />
+       <Route path="/uploadImage" exact component={UploadImage} />
+
+      </Router>
     </div>
+    </AmplifyAuthenticator>
+
   );
 }
 
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,105 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const newOnCreatePicture = /* GraphQL */ `
+  subscription NewOnCreatePicture {
+    newOnCreatePicture {
+      id
+      name
+      owner
+      labels
+      file {
+        bucket
+        region
+        key
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const newOnUpdatePicture = /* GraphQL */ `
+  subscription NewOnUpdatePicture {
+    newOnUpdatePicture {
+      id
+      name
+      owner
+      labels
+      file {
+        bucket
+        region
+        key
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const newOnDeletePicture = /* GraphQL */ `
+  subscription NewOnDeletePicture {
+    newOnDeletePicture {
+      id
+      name
+      owner
+      labels
+      file {
+        bucket
+        region
+        key
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onCreatePicture = /* GraphQL */ `
+  subscription OnCreatePicture {
+    onCreatePicture {
+      id
+      name
+      owner
+      labels
+      file {
+        bucket
+        region
+        key
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdatePicture = /* GraphQL */ `
+  subscription OnUpdatePicture {
+    onUpdatePicture {
+      id
+      name
+      owner
+      labels
+      file {
+        bucket
+        region
+        key
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeletePicture = /* GraphQL */ `
+  subscription OnDeletePicture {
+    onDeletePicture {
+      id
+      name
+      owner
+      labels
+      file {
+        bucket
+        region
+        key
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
